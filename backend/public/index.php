780b6f5279927437792106b5f524755513862a0c
Dgabriel-dev <joaofgd123@gmail.com>
2026-08-02 17:32:43 -0300

    debug: unconditional die at top of public/index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+header('Content-Type: application/json');
+echo json_encode([
+    'file' => __FILE__,
+    'script_name' => $_SERVER['SCRIPT_NAME'] ?? 'N/A',
+    'request_uri' => $_SERVER['REQUEST_URI'] ?? 'N/A',
+    'query_string' => $_SERVER['QUERY_STRING'] ?? 'N/A',
+    'is_vercel' => getenv('VERCEL') || getenv('VERCEL_ENV'),
+]);
+exit;
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
