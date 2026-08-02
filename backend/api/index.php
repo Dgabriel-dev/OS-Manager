f52a8e42272466c0c03ca3b4fdf2f1cffec11ff7
Dgabriel-dev <joaofgd123@gmail.com>
2026-08-02 15:10:46 -0300

    debug: enable error display temporarily

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,7 +1,8 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors', '0');
+ini_set('display_errors', '1');
+ini_set('log_errors', '0');
 
 $isVercel = getenv('VERCEL') || getenv('VERCEL_ENV');
 
